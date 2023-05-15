ef60e23dd01c635e070b437662f2517b36811fc8
Rolando Delgado <rolandodelgado710@gmail.com>
2023-05-14 21:47:40 -0400

    admin productos y estados de productos

--- productos/models.py
+++ productos/models.py
@@ -5,3 +5,6 @@ class Producto(models.Model):
     id = djmodels.ObjectIdField(primary_key=True)
     nombre = models.CharField(max_length=100)
     precio = models.IntegerField(default=0) # Puede ser mas adecuado usar DecimalField
+
+    def __str__(self):
+        return f'{self.id} - {self.nombre}'
